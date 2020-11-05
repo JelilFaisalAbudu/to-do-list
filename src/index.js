3e6ea94004066927d4c9c068a4edd095193422cb
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-11-05 15:33:45 +0300

    finish the webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './index.scss'
+
 function component() {
   const element = document.createElement('div');
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
-        test: /\.css$/,
-        use: [{
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          }
-        ],
-      },
+    rules: [
       {
         test: /\.scss$/,
         use: [{
