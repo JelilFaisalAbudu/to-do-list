16a9ea3838a94fd4498ef581095c32f8e4bb0bb9
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-11-06 17:22:12 +0300

    Add to do button to Projects page

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -2,10 +2,15 @@ function ProjectUI(project) {
   const body = document.querySelector('body');
   const firstP = document.createElement('p');
   const secondP = document.createElement('p');
-  firstP.innerText = project.title;
-  secondP.innerText = project.description;
-  body.appendChild(firstP);
-  body.appendChild(secondP);
+  const firstDiv = document.createElement('div');
+  const addToDoBtn = document.createElement('button');
+  addToDoBtn.innerText = 'Add To Do';
+  firstP.innerText = `The project Title is: ${project.title}`;
+  secondP.innerText = `The project description is: ${project.description}`;
+  firstDiv.appendChild(firstP);
+  firstDiv.appendChild(secondP);
+  firstDiv.appendChild(addToDoBtn);
+  body.appendChild(firstDiv);
 }
 
 export default ProjectUI;
--- src/modules/ProjectFormRenderer.js
+++ src/modules/ProjectFormRenderer.js
@@ -7,14 +7,15 @@ function renderer() {
   document.querySelector('.new-project-btn').remove();
 
   const createBtn = document.querySelector('.submit-btn');
-
-  createBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const projectTitle = document.querySelector('.form-control').value;
-    const projectDescription = document.querySelectorAll('.form-control')[1].value;
-    const newProject = new Project(projectTitle, projectDescription);
-    ProjectUI(newProject);
-  });
+  if (document.querySelector('.project-form')) {
+    createBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const projectTitle = document.querySelector('.form-control').value;
+      const projectDescription = document.querySelectorAll('.form-control')[1].value;
+      const newProject = new Project(projectTitle, projectDescription);
+      ProjectUI(newProject);
+    });
+  }
 }
 
 export default renderer;
