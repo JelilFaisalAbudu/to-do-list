0df94e41f9d05daf5f0510f7687f670915543a55
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-11-06 18:33:15 +0300

    Delete unnecessary methods from constructor

--- src/modules/AddToProject.js
+++ src/modules/AddToProject.js
@@ -4,26 +4,6 @@ class Project {
     this.description = description;
     this.toDoList = [];
   }
-
-  getTitle() {
-    return this.title;
-  }
-
-  getDescription() {
-    return this.description;
-  }
-
-  removeToDo(index) {
-    this.toDoList.splice(index, 1);
-  }
-
-  getToDoList() {
-    return this.toDoList;
-  }
-
-  addToDo(toDo) {
-    this.toDoList.push(toDo);
-  }
 }
 
 export default Project;
--- src/modules/addToDo.js
+++ src/modules/addToDo.js
@@ -6,46 +6,6 @@ class Todo {
     this.priority = priority;
     this.completed = completed;
   }
-
-  title() {
-    return this.title;
-  }
-
-  description() {
-    return this.description;
-  }
-
-  dueDate() {
-    return this.dueDate;
-  }
-
-  priority() {
-    return this.priority;
-  }
-
-  completed() {
-    return this.completed;
-  }
-
-  editTitle(title) {
-    this.title = title;
-  }
-
-  editDescription(desc) {
-    this.description = desc;
-  }
-
-  editDueDate(dueDate) {
-    this.dueDate = dueDate;
-  }
-
-  editPriority(priority) {
-    this.priority = priority;
-  }
-
-  editCompleted(completed) {
-    this.completed = completed;
-  }
 }
 
 export default Todo;
